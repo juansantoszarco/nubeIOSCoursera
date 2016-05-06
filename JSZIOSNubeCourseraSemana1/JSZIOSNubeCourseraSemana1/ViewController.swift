a6dc4922e8a06797e77ed956340fce749b65b6a7
Juan Antonio de los Santos <juansantoszarco@gmail.com>
2016-05-06 20:30:10 +0200

    Initial Commit Semana 1

---
+++ JSZIOSNubeCourseraSemana1/JSZIOSNubeCourseraSemana1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSZIOSNubeCourseraSemana1
+//
+//  Created by Juan Antonio de los Santos on 06/05/16.
+//  Copyright (c) 2016 Juan Antonio de los Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

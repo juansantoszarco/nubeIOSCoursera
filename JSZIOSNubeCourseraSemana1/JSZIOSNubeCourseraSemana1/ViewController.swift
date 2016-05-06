ebfa8fdaa401733fef1fa2a2748a1be997df1535
Juan Antonio de los Santos <juansantoszarco@gmail.com>
2016-05-06 21:05:27 +0200

    Añadidos 2 outlets y autolayout

--- JSZIOSNubeCourseraSemana1/JSZIOSNubeCourseraSemana1/ViewController.swift
+++ JSZIOSNubeCourseraSemana1/JSZIOSNubeCourseraSemana1/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var resultTextField: UITextView!
+    
+    @IBAction func selectButton(sender: UIButton) {
+        let request = NSString (format: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:%@", inputTextField.text!)
+        print(request)
+        let url = NSURL (string: request as String)
+        let session = NSURLSession.sharedSession()
+        let bloque = { (datos : NSData?, response : NSURLResponse?, error : NSError?) -> Void in
+            let texto = NSString (data : datos!, encoding : NSUTF8StringEncoding)
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.resultTextField.text = texto! as String
+            })
+            
+            print(texto!)
+        }
+        let download = session.dataTaskWithURL(url!, completionHandler:  bloque)
+        download.resume()
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
